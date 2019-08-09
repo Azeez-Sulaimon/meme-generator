871c4c11c795063c7e0105d7e5c3aa4da42d5b03
omoade <seunomoade@gmail.com>
2019-08-09 18:20:03 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+import "./App.css";
+import Header from "./Header";
+import MemeGenerator from "./MemeGenerator";
+
+class App extends Component {
+    
+     constructor(){
+        super()
+        this.state={
+            
+        }
+    };
+    
+    render(){
+        return(
+            <div>
+            <Header />
+            <MemeGenerator />
+            </div>
+        )
+    }
+}
+
+export default App;
